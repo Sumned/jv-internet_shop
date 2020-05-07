21a4758a54a79b2b353d2c7c28bf049ff40f65bb
Sumned <47605197+Sumned@users.noreply.github.com>
2020-05-07 13:58:23 +0300

    added solution (#9)
    * added solution
    
    * fixed
    
    * fixed

--- src/main/java/mate/academy/internetshop/controller/LoginController.java
+++ src/main/java/mate/academy/internetshop/controller/LoginController.java
@@ -10,8 +10,11 @@ import mate.academy.internetshop.exceptions.AuthenticationException;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.security.AuthenticationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LoginController extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(LoginController.class.getName());
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private final AuthenticationService authenticationService =
             (AuthenticationService) INJECTOR.getInstance(AuthenticationService.class);
@@ -27,11 +30,14 @@ public class LoginController extends HttpServlet {
             throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
+        LOGGER.info("User " + login + " trying to login");
         try {
             User user = authenticationService.login(login, password);
+            LOGGER.info("User " + login + " successfully logged in");
             HttpSession session = req.getSession();
             session.setAttribute("user_id", user.getId());
         } catch (AuthenticationException e) {
+            LOGGER.error("Login error ", e);
             req.setAttribute("errorMsg", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
             return;
